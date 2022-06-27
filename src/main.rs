9d97281bd6ffdae894f8052c91ea32d1d761fdb2
Peer Springstübe <kratenko@garstig.org>
2022-06-27 20:57:47 +0200

    Make it more rusty - and not that panicking.

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,14 @@ pub mod op {
     pub const FIN: u8 = 0xff;
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum RuntimeError {
+    EndOfProgram,
+    InvalidOperation(u8),
+    StackUnderflow,
+    StackOverflow,
+}
+
 /// The virtual machine itself.
 ///
 /// Holds the state during execution of programs.
@@ -45,18 +53,41 @@ pub struct VM {
 }
 
 impl VM {
-    /// Reads the next byte from the bytecode, increase programm counter, and return byte.
-    fn fetch_u8(&mut self, pgm: &[u8]) -> u8 {
-        if self.pc >= pgm.len() {
-            panic!("End of program exceeded");
+    pub fn new(stack_size: usize) -> VM{
+        VM{
+            stack: Vec::with_capacity(stack_size),
+            pc: 0,
+            op_cnt: 0
         }
-        let v = pgm[self.pc];
-        self.pc += 1;
-        v
+    }
+    
+    /// Tries and pops a value from value stack, respecting frame base.
+    fn pop(&mut self) -> Result<i64, RuntimeError> {
+        self.stack.pop().ok_or(RuntimeError::StackUnderflow)
+    }
+
+    /// Tries and pushes a value to value stack, respecting stack size.
+    fn push(&mut self, v: i64) -> Result<(), RuntimeError> {
+        if self.stack.len() < self.stack.capacity() {
+            self.stack.push(v);
+            Ok(())
+        } else {
+            Err(RuntimeError::StackOverflow)
+        }
+    }
+
+    /// Reads the next byte from the bytecode, increase programm counter, and return byte.
+    fn fetch_u8(&mut self, pgm: &[u8]) -> Result<u8, RuntimeError> {
+        if let Some(v) = pgm.get(self.pc) {
+            self.pc += 1;
+            Ok(*v)
+        } else {
+            Err(RuntimeError::EndOfProgram)
+        }
     }
 
     /// Executes a program (encoded in bytecode).
-    pub fn run(&mut self, pgm: &[u8]) {
+    pub fn run(&mut self, pgm: &[u8]) -> Result<(), RuntimeError> {
         // initialise the VM to be in a clean start state:
         self.stack.clear();
         self.pc = 0;
@@ -67,7 +98,7 @@ impl VM {
             // Log the vm's complete state, so we can follow what happens in console:
             println!("{:?}", self);
             // Fetch next opcode from program (increases program counter):
-            let opcode = self.fetch_u8(pgm);
+            let opcode = self.fetch_u8(pgm)?;
             // We count the number of instructions we execute:
             self.op_cnt += 1;
             // If we are done, break loop and stop execution:
@@ -75,43 +106,46 @@ impl VM {
                 break;
             }
             // Execute the current instruction (with the opcode we loaded already):
-            self.execute_op(pgm, opcode);
+            self.execute_op(pgm, opcode)?;
         }
         // Execution terminated. Output the final state of the VM:
         println!("Terminated!");
         println!("{:?}", self);
+        Ok(())
     }
 
     /// Executes an instruction, using the opcode passed.
     ///
     /// This might load more data from the program (opargs) and
     /// manipulate the stack (push, pop).
-    fn execute_op(&mut self, pgm: &[u8], opcode: u8) {
+    fn execute_op(&mut self, pgm: &[u8], opcode: u8) -> Result<(), RuntimeError> {
         println!("Executing op 0x{:02x}", opcode);
         match opcode {
             op::NOP => {
                 println!("  NOP");
                 // do nothing
+                Ok(())
             },
             op::POP => {
                 println!("  POP");
-                let v = self.stack.pop().unwrap();
+                let v = self.pop()?;
                 println!("  dropping value {}", v);
+                Ok(())
             },
             op::PUSH_U8 => {
                 println!("  PUSH_U8");
-                let v = self.fetch_u8(pgm);
+                let v = self.fetch_u8(pgm)?;
                 println!("  value: {}", v);
-                self.stack.push(v as i64);
+                self.push(v as i64)
             },
             op::ADD => {
                 println!("  ADD");
-                let a = self.stack.pop().unwrap();
-                let b = self.stack.pop().unwrap();
-                self.stack.push(a + b);
+                let a = self.pop()?;
+                let b = self.pop()?;
+                self.push(a + b)
             },
             _ => {
-                panic!("unknown opcode!");
+                Err(RuntimeError::InvalidOperation(opcode))
             }
         }
     }
@@ -122,11 +156,14 @@ fn main() {
     // We just hardcode the bytes in an array here:
     let pgm = [op::NOP, op::PUSH_U8, 100, op::PUSH_U8, 77, op::ADD, op::POP, 0xff];
     // Crate our VM instance.
-    let mut vm = VM{
-        stack: Vec::with_capacity(100),
-        pc: 0,
-        op_cnt: 0
-    };
+    let mut vm = VM::new(100);
     // Execute the program in our VM:
-    vm.run(&pgm);
+    match vm.run(&pgm) {
+        Ok(_) => {
+            println!("Execution successful.")
+        }
+        Err(e) => {
+            println!("Error during execution: {:?}", e);
+        }
+    }
 }
